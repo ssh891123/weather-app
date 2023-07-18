e12205215ee93355fc57bd955948615ea3a2d8cb
ssh891123 <ssh891123@gmail.com>
2023-07-18 22:33:06 +0900

    [feat] 온도 기능 추가
    - 체감온도, 최저온도, 최고온도 출력 기능 추가

--- src/CurrentWeather/CurrentWeather.jsx
+++ src/CurrentWeather/CurrentWeather.jsx
@@ -3,11 +3,15 @@ import { WeatherContext } from '../WeatherProvider/WeatherProvider';
 import CurrentWeatherIcon from '../CurrentWeatherIcon/CurrentWeatherIcon';
 
 function CurrentWeather() {
+    // name - 도시
+    // temp - 현재 온도
     const {name, temp, weatherState } = useContext(WeatherContext);
+
+    //nbsp: 공백
     return(
         <div className='weather'> 
             {name} &nbsp;/
-            <CurrentWeatherIcon weatherState={weatherState}/>
+            <CurrentWeatherIcon weatherState={weatherState} />
             <span>{temp}&deg;</span>
         </div>
     )
--- src/WeatherApp.jsx
+++ src/WeatherApp.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import CurrentWeather from './CurrentWeather/CurrentWeather';
 import './style.css';
+import TempInfo from './TempInfo/TempInfo';
 
 function WeatherApp() {
     return(
         <>
-        <div classNmae = 'container'>
+        <div className = 'container'>
             <CurrentWeather/>
+            <TempInfo/>
         </div>
         </>
     )
